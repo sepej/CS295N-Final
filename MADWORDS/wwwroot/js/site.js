ad4073b374afd6768f2300bd362ba69e30083303
jasii <josephsepe@gmail.com>
2020-12-05 00:22:56 -0800

    Fixed story objects

--- MADWORDS/wwwroot/js/site.js
+++ MADWORDS/wwwroot/js/site.js
@@ -2,12 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-$("#submitbtn").on("click", function () {
-    var a = '';
-    $('.word').each(function () {
-        a += this.value+",";
-    });
-    $('#words').val(a);
-}
-);
