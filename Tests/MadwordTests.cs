6efb2c3df6946d59955ccde22cf1a97d8d99da53
jasii <josephsepe@gmail.com>
2020-12-09 01:53:34 -0800

    Added Tests

---
+++ Tests/MadwordTests.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class MadwordsTests
+    {
+        [Fact]
+        public void AddMadwordTest()
+        {
+            
+        }
+    }
+}
