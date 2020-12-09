3696199fb7216406391cccf04e92add7735c25d1
jasii <josephsepe@gmail.com>
2020-12-09 02:03:43 -0800

    Added Tests for adding madwords

--- Tests/MadwordTests.cs
+++ Tests/MadwordTests.cs
@@ -1,5 +1,8 @@
 using System.Linq;
 using Xunit;
+using MADWORDS.Controllers;
+using MADWORDS.Models;
+using MADWORDS.Repos;
 
 namespace Tests
 {
@@ -8,7 +11,25 @@ namespace Tests
         [Fact]
         public void AddMadwordTest()
         {
-            
+            // Arrange
+            var fakeRepo = new FakeMadwordRepository();
+            var controller = new MadwordController(fakeRepo);
+            var madword = new Madword()
+            {
+                MadwordTitle = "New Madword Title",
+                MadwordTopic = "New Topic",
+                AuthorName = "Author",
+                MadwordRating = 5,
+                MadwordText = "MADWORD TEXT"
+            };
+
+            // Act
+            controller.Create(madword);
+
+            // Assert
+            // Ensure that the review was added to the repository
+            var retrievedMadword = fakeRepo.Madwords.ToList()[0];
+            Assert.Equal(0,System.DateTime.Now.Date.CompareTo(retrievedMadword.MadwordDate.Date));
         }
     }
 }
